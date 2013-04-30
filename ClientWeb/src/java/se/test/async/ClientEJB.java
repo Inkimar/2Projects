8a978bce215b8690c100cd53bc374a9a73ca2250
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-04-30 11:32:56 +0200

    Test : using old-school web.xml instead of annotation on the servlet.

---
+++ ClientWeb/src/java/se/test/async/ClientEJB.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.test.async;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Properties;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ingimar
+ */
+public class ClientEJB extends HttpServlet {
+
+   
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet calling EJB </title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Calling EJB ->  " + this.outreach() + " <- </h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {            
+            out.close();
+        }
+    }
+    
+    private String outreach () {
+        String response = " No response form Bean";
+        try {
+            Properties jndiProps = new Properties();
+            jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
+            jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+            jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
+            jndiProps.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+            Context ctx = new InitialContext(jndiProps);
+            @SuppressWarnings("unused")
+            AsyncRemote bean = (AsyncRemote) ctx.lookup("java:global/Async/AsyncBean");
+            response = bean.businessMethod();
+           
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP
+     * <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP
+     * <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+}
