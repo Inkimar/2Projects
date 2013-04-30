cfd858d29adc489ef406a3a1149d97895081154c
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-04-30 17:05:27 +0200

    new package in domain

--- ClientEJB/src/se/test/async/J2SEClientAsync.java
+++ ClientEJB/src/se/test/async/J2SEClientAsync.java
@@ -2,12 +2,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package clientejb;
+package se.test.async;
 
 import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
-import zanzibar.AsyncRemote;
+import se.test.async.AsyncRemote;
 
 /**
  * Lägg jarar i den ordningen:
@@ -25,8 +25,7 @@ public class J2SEClientAsync {
             jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
             jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
             jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
-            jndiProps.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.10.163"); // 192.168.10.163 , 127.0.0.1
             Context ctx = new InitialContext(jndiProps);
             @SuppressWarnings("unused")
             AsyncRemote bean = (AsyncRemote) ctx.lookup("java:global/Async/AsyncBean");
