c5aaa406fe26f08a787a3f4c2af26dbcaa3657f5
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-05-08 09:43:49 +0200

    Removed stuff from java. Simplified property-file.

--- ClientEJB/src/se/test/async/J2SEClientAsync.java
+++ ClientEJB/src/se/test/async/J2SEClientAsync.java
@@ -18,20 +18,28 @@ import se.test.async.AsyncRemote;
  */
 public class J2SEClientAsync {
 
+    /**
+     * (1) Kan enbart köra med properties-fil på vissa ( därav kommenterat bort Properties här )
+     * (2) Måste använda Properties på vissa ( går ej med properties-fil )
+     * 
+     * @param args 
+     */
     public static void main(String[] args) {
-        System.out.println("Hej världen, hur mår du idag! ");
+        System.out.println("Start: Hej världen!");
         try {
-            Properties jndiProps = new Properties();
-            jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
-            jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
-            jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "192.168.10.163"); // 192.168.10.163 , 127.0.0.1
-            Context ctx = new InitialContext(jndiProps);
+//            Properties jndiProps = new Properties();
+//            jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
+//            jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+//            jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            //jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "172.16.34.31"); // 172.16.34.31 , 192.168.10.163 , 127.0.0.1
+            //Context ctx = new InitialContext(jndiProps);
+            Context ctx = new InitialContext();
             @SuppressWarnings("unused")
             AsyncRemote bean = (AsyncRemote) ctx.lookup("java:global/Async/AsyncBean");
             System.out.println("Response coming from bean :"+bean.businessMethod());
         } catch (Exception e) {
             e.printStackTrace();
         }
+        System.out.println("END: Hej världen!");
     }
 }
