eaa5366850c40684e2e558825f843ff2a39e3df4
Ingimar Erlingsson <ingimar.erlingsson@nrm.se>
2013-05-08 09:50:58 +0200

    Removed stuff from java.

--- ClientWeb/src/java/se/test/async/ClientEJB.java
+++ ClientWeb/src/java/se/test/async/ClientEJB.java
@@ -6,6 +6,7 @@ package se.test.async;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 import java.util.Properties;
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -26,13 +27,13 @@ public class ClientEJB extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>Servlet calling EJB </title>");            
             out.println("</head>");
             out.println("<body>");
+            out.println("<h1>Time on Client ->  " + new Date()+ " <- </h1>");
             out.println("<h1>Calling EJB ->  " + this.outreach() + " <- </h1>");
             out.println("</body>");
             out.println("</html>");
@@ -45,11 +46,10 @@ public class ClientEJB extends HttpServlet {
         String response = " No response form Bean";
         try {
             Properties jndiProps = new Properties();
-            jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
-            jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
-            jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
-            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "127.0.0.1");
-            jndiProps.setProperty("org.omg.CORBA.ORBInitialPort", "3700");
+//            jndiProps.put("java.naming.factory.initial", "com.sun.enterprise.naming.impl.SerialInitContextFactory");
+//            jndiProps.put("java.naming.factory.url.pkgs", "com.sun.enterprise.naming");
+//            jndiProps.put("java.naming.factory.state", "com.sun.corba.ee.impl.presentation.rmi.JNDIStateFactoryImpl");
+            jndiProps.setProperty("org.omg.CORBA.ORBInitialHost", "172.16.34.31"); // 172.16.34.31 , 127.0.0.1
             Context ctx = new InitialContext(jndiProps);
             @SuppressWarnings("unused")
             AsyncRemote bean = (AsyncRemote) ctx.lookup("java:global/Async/AsyncBean");
